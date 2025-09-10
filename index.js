14d019c81b25d14b890043c19ddda02b5a832ff4
AmanMomin2207 <amanchandsomomin7860@gmail.com>
2025-09-10 11:35:13 +0530

    change project design

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
   fetch("projects.json")
-      .then(res => res.json())
-      .then(projects => {
-        const container = document.getElementById("project-info");
+    .then((res) => res.json())
+    .then((projects) => {
+      const container = document.getElementById("project-info");
 
-        projects.map(project => {
-            const card = document.createElement("div");
-            card.className = "lg:w-1/4 mb-10 px-4 py-4 shadow-2xl shadow-sky-300";
-
-            card.innerHTML = `
-                <div class="rounded-lg h-64 overflow-hidden">
-                  <img alt="content" class="object-contain object-center h-full w-full " src=${project.img}>
-                </div>
-                <h2 class="title-font text-2xl font-medium text-white mt-6 mb-3">${project.title}</h2>
-                <p class="leading-relaxed text-base">
-                  <ul>
-                    <li class="font-bold text-xl m-4">Technology :</li>
-                    <li class="text-md font-bold">${project.techStack}</li>
-                  </ul>
-                </p>
-                <a href=${project.link}
-                  target="_blank"
-                >
-                  <button class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded" >Click Here</button>
-                </a>  
-            `
-            container.appendChild(card);
-        });
-      })
-      .catch(err => console.error("Error loading projects:", err));
       
-    fetch("Achievements.json")
-      .then(res => res.json())
-      .then(Achievements => {
-        const container = document.getElementById('achievement-info');
 
-        Achievements.map(Achievement => {
-          const card = document.createElement("div");
-          card.className = "p-4 lg:w-1/4";
+      projects.map((project) => {
+        
+        const card = document.createElement("div");
+        card.className = "lg:w-1/4 mb-10 px-4 py-4 shadow-2xl shadow-sky-300 bg-blue-900 rounded-lg";
 
-          card.innerHTML = `
+        const links = project.link ? project.link : "#";
+
+        card.innerHTML = `
+                <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img class="lg:h-48 md:h-36 w-full object-cover object-center" src=${project.img} alt="blog">
+                  <div class="p-2 pt-4 flex justify-around items-center">
+                    <a href = ${links}
+                     class="text-indigo-500 inline-flex items-center" target="_blank">🚀 Live Project </a>
+                    <a href = ${project.GitHub_Link} class="text-indigo-500 inline-flex items-center" target="_blank">💻 GitHub Link </a>
+                  </div>
+                  <div class="p-6">
+                    <h1 class="title-font text-lg font-medium text-white mb-3">Tech Stack : </h1>
+                    <h2 class=" text-sm title-font text-white mb-2 pb-4 font-bold">${project.techStack}</h2>
+                    <p class="leading-relaxed mb-3">${project.description}</p>
+                  </div>
+                  
+                </div>
+
+
+            `;
+        container.appendChild(card);
+      });
+    })
+    .catch((err) => console.error("Error loading projects:", err));
+
+  fetch("Achievements.json")
+    .then((res) => res.json())
+    .then((Achievements) => {
+      const container = document.getElementById("achievement-info");
+
+      Achievements.map((Achievement) => {
+        const card = document.createElement("div");
+        card.className = "p-4 lg:w-1/4";
+
+        card.innerHTML = `
             <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 rounded-lg overflow-hidden text-center relative">
               <h1 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">${Achievement.title}</h2>
               <h2 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">${Achievement.Rank}<small>st</small> Place </h1>
@@ -51,10 +57,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 </svg>
               </a>
             </div>
-          `
+          `;
 
-          container.appendChild(card);
-        });
-      })
-    .catch(err => console.error("Error loading projects:", err));
-})
+        container.appendChild(card);
+      });
+    })
+    .catch((err) => console.error("Error loading projects:", err));
+});
